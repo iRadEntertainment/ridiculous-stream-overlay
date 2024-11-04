8c19b887601d3299e6a8228c95a98b336139f780
Dario De Vita <devitadario@gmail.com>
2024-11-04 02:49:13 +0100

    fix: Mouse tracker and debug view fix for window decoration and HSplit boxes grabber detection

--- classes/RSMouseTracker.gd
+++ classes/RSMouseTracker.gd
@@ -12,6 +12,7 @@ var is_active := true:
 
 var is_gui_active := false
 var is_on_a_control_node := false
+var hovered_control_node: Control
 var is_on_a_window := false
 var is_on_title_bar := false
 var is_on_window_decoration := false
@@ -47,8 +48,6 @@ func is_pixel_transparent(pos: Vector2) -> bool:
 	return !get_window().get_texture().get_image().get_pixelv(pos).a > 0
 
 func is_m_pos_in_control_nodes(pos: Vector2) -> bool:
-	Rect2i().has_point(pos)
-	
 	if !main.get_rect().has_point(pos):
 		return false
 	for ctr: Control in get_tree().get_nodes_in_group("UI"):
@@ -58,7 +57,22 @@ func is_m_pos_in_control_nodes(pos: Vector2) -> bool:
 		if ctr_angle != 0:
 			pos_rotated = (pos - ctr.global_position).rotated(-ctr_angle) + ctr.global_position
 		
+		if ctr is HSplitContainer:
+			if ctr.get_child_count() != 2: continue
+			var ctr_left: Control = ctr.get_child(0)
+			var ctr_right: Control = ctr.get_child(1)
+			if !ctr_left.visible or !ctr_right.visible:
+				continue
+			var grabber_rect := ctr.get_global_rect()
+			grabber_rect.position.x = ctr_left.size.x
+			grabber_rect.size.x -= ctr_left.size.x + ctr_right.size.x
+			if grabber_rect.has_point(pos_rotated):
+				hovered_control_node = ctr
+				return true
+			continue
+		
 		if ctr.get_global_rect().has_point(pos_rotated):
+			hovered_control_node = ctr
 			return true
 	return false
 
@@ -87,14 +101,17 @@ func is_m_pos_in_title(pos: Vector2) -> bool:
 func is_m_pos_on_window_decoration(pos: Vector2) -> bool:
 	if DisplayServer.window_get_flag(DisplayServer.WINDOW_FLAG_BORDERLESS):
 		return false
-	var window_rect := Rect2i(Vector2i(), DisplayServer.window_get_size())
-	
+	var window_rect := Rect2i(main.get_rect())
 	var title_size: Vector2i = DisplayServer.window_get_title_size("App")
-	var decorations_size := DisplayServer.window_get_size_with_decorations()
-	var decorations_pos := Vector2i()
-	@warning_ignore("integer_division")
-	decorations_pos.x = -(decorations_size.x - window_rect.size.x)/2
-	decorations_pos.y = - title_size.y - 2 #px
+	var decorations_offset := Vector2i.ONE * 8
+	
+	var decorations_size := window_rect.size
+	decorations_size.y += title_size.y
+	decorations_size += decorations_offset * 2
+	
+	var decorations_pos := -decorations_offset
+	decorations_pos.y -= title_size.y
+	
 	var window_rect_decorations := Rect2i(decorations_pos, decorations_size)
 	
 	return window_rect_decorations.has_point(pos) and not window_rect.has_point(pos)
--- instances/debug_view.gd
+++ instances/debug_view.gd
@@ -3,12 +3,17 @@ extends Control
 @onready var lb_fps: Label = %lb_fps
 @onready var lb_tot_obj: Label = %lb_tot_obj
 @onready var lb_mouse_pos: Label = %lb_mouse_pos
-@onready var cl_hover: ColorRect = %cl_hover
+@onready var lb_hover: Label = %lb_hover
 @onready var cl_title_flag: ColorRect = %cl_title_flag
 @onready var cl_decoration: ColorRect = %cl_decoration
 @onready var cl_win: ColorRect = %cl_win
 
+var lb_style: StyleBoxFlat
+
 var main: RSMain
+
+func _ready() -> void:
+	lb_style = lb_hover.get_theme_stylebox("normal")
 
 func start(_main: RSMain):
 	hide()
@@ -26,13 +31,13 @@ func _process(_d: float) -> void:
 	if not main.mouse_tracker: return
 	if !is_visible_in_tree(): return
 	
-	lb_mouse_pos.text = str(main.mouse_tracker.m_pos)
+	lb_mouse_pos.text = "%4d, %4d" % [main.mouse_tracker.m_pos.x , main.mouse_tracker.m_pos.y]
 	cl_win.color = Color.SEA_GREEN if main.mouse_tracker.is_on_a_window else Color.CRIMSON
-	cl_hover.color = Color.SEA_GREEN if main.mouse_tracker.is_on_a_control_node else Color.CRIMSON
+	lb_style.bg_color = Color.SEA_GREEN if main.mouse_tracker.is_on_a_control_node else Color.CRIMSON
+	lb_hover.text = "is on UI element"
+	lb_hover.has_theme_stylebox_override("normal")
+	if main.mouse_tracker.is_on_a_control_node:
+		lb_hover.text = "is on UI element: %s" % main.mouse_tracker.hovered_control_node.name
 	cl_title_flag.color = Color.SEA_GREEN if main.mouse_tracker.is_on_title_bar else Color.CRIMSON
 	cl_decoration.color = Color.SEA_GREEN if main.mouse_tracker.is_on_window_decoration else Color.CRIMSON
 	lb_fps.text = "fps %s" % [Engine.get_frames_per_second()]
-
-
-func _on_btn_launch_game_pressed() -> void: main.launch_game(true)
-func _on_btn_close_game_pressed() -> void: main.launch_game(false)
