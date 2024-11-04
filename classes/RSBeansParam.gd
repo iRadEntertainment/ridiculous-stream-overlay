5abfcae991c143775da7e310c76d01df2ff78e13
Dario De Vita <devitadario@gmail.com>
2024-11-04 02:44:31 +0100

    feat: set default benas param

--- classes/RSBeansParam.gd
+++ classes/RSBeansParam.gd
@@ -3,17 +3,17 @@ extends Resource
 class_name RSBeansParam
 
 
-var img_paths : Array = []
-var sfx_paths : Array = []
-var spawn_range : Array = [3, 5]
-var coll_layer : int = 4
-var coll_mask : int = 5
-var sfx_volume : int = 0
-var is_destroy : bool
-var is_pickable : bool
-var is_poly_fracture : bool
-var scale : Vector2 = Vector2.ONE
-var destroy_shard_params : RSBeansParam
+var img_paths: Array = []
+var sfx_paths: Array = []
+var spawn_range: Array = [3, 5]
+var coll_layer: int = 4
+var coll_mask: int = 5
+var sfx_volume: int = 0
+var is_destroy: bool = true
+var is_pickable: bool = true
+var is_poly_fracture: bool = false
+var scale: Vector2 = Vector2.ONE
+var destroy_shard_params: RSBeansParam
 
 
 func to_dict() -> Dictionary:
@@ -53,5 +53,3 @@ static func from_json(d: Dictionary) -> RSBeansParam:
 	if d["destroy_shard_params"] != null:
 		param.destroy_shard_params = RSBeansParam.from_json(d["destroy_shard_params"])
 	return param
-
-
