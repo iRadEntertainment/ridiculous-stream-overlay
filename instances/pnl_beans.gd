afb0b53f59c86185496558389648ecc5190898ee
AnihanShard <catsandkai@gmail.com>
2024-11-11 16:21:24 -0800

    feat: ani wants to do panel stuff for beans!!

---
+++ instances/pnl_beans.gd
@@ -0,0 +1,17 @@
+extends PanelContainer
+
+
+func _on_btn_test_beans_pressed():
+	push_error("We need more beans, bro.")
+	pass # Replace with function body.
+
+
+func _on_btn_save_beans_pressed():
+	push_error("Lets save some of these bean babies for later.")
+	pass # Replace with function body.
+
+
+func _on_btn_load_beans_pressed():
+	push_error("Sweet! Home-cooked beans I didn't have to cook myself.")
+	pass # Replace with function body.
+s
